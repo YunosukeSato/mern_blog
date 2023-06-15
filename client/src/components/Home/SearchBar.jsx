cc166be5d5ce6ecc74137c207d91b20b86a5309b
Yunosuke Sato <yunosukesato04@gmail.com>
2023-06-15 16:14:58 -0700

    search bar to filter blogs

--- client/src/components/Home/SearchBar.jsx
+++ client/src/components/Home/SearchBar.jsx
@@ -6,39 +6,36 @@ import {
   CDropdownItem,
 } from "@coreui/react";
 
-function SearchBar({ formSubmit, value, handleSearchKey, clearSearch }) {
+function SearchBar({ formSubmit, value, handleSearchKey }) {
   let dropdownValue = "";
-  if (value) {
-    dropdownValue = value;
-  } else if (value == "All") {
+  if (value == "All") {
     dropdownValue = "Category";
+  } else if (value) {
+    dropdownValue = value;
   }
 
   return (
-    <div className="bg-[#f0f0f0] w-fit mt-10 mr-auto mb-16 ml-auto p-2 rounded-md">
-      <form className="flex items-center" onSubmit={formSubmit}>
-        {/* <input
-          className="bg-[#f0f0f0] outline-none cursor-pointer"
-          type="text"
-          placeholder="Search by category"
-          value={value}
-          onChange={handleSearchKey}
-        /> */}
-        <CDropdown>
-          <CDropdownToggle color="secondary">{dropdownValue}</CDropdownToggle>
-          <CDropdownMenu>
-            <CDropdownItem onClick={handleSearchKey}>Development</CDropdownItem>
-            <CDropdownItem onClick={handleSearchKey}>Shoping</CDropdownItem>
-            <CDropdownItem onClick={handleSearchKey}>Travel</CDropdownItem>
-            <CDropdownItem onClick={handleSearchKey}>All</CDropdownItem>
-          </CDropdownMenu>
-        </CDropdown>
-        {value && <span onClick={clearSearch}>X</span>}
-        <button className="outline-none border-none py-1 px-4 rounded-md bg-[#0f52ba] text-[#fff]">
-          Search
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="bg-[#f0f0f0] w-fit mt-10 mr-auto mb-16 ml-auto p-2 rounded-md">
+        <form className="flex items-center" onSubmit={formSubmit}>
+          <CDropdown>
+            <CDropdownToggle color="secondary">{dropdownValue}</CDropdownToggle>
+            {/* <CDropdownToggle color="secondary">Dropdown menu</CDropdownToggle> */}
+            <CDropdownMenu>
+              <CDropdownItem onClick={handleSearchKey}>
+                Development
+              </CDropdownItem>
+              <CDropdownItem onClick={handleSearchKey}>Shoping</CDropdownItem>
+              <CDropdownItem onClick={handleSearchKey}>Travel</CDropdownItem>
+              <CDropdownItem onClick={handleSearchKey}>All</CDropdownItem>
+            </CDropdownMenu>
+          </CDropdown>
+          <button className="outline-none border-none py-1 px-4 rounded-md bg-[#0f52ba] text-[#fff] ml-5">
+            Search
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
 
