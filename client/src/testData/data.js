c2ddf2a1aab7193116edbc578cc63cd3ef61c30f
Yunosuke Sato <yunosukesato04@gmail.com>
2023-06-15 00:40:49 -0700

    dammy blogs

---
+++ client/src/testData/data.js
@@ -0,0 +1,57 @@
+export const blogList = [
+  {
+    id: 1,
+    title: "First blog title 111",
+    category: "development",
+    description:
+      "<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>",
+    authorName: "Yunosuke Sato",
+    authorAvatar: "",
+    createdAt: "June 11, 2023",
+    cover: "",
+  },
+  {
+    id: 2,
+    title: "Second blog title 222",
+    category: "development",
+    description:
+      "<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>",
+    authorName: "Yunosuke Sato",
+    authorAvatar: "",
+    createdAt: "June 11, 2023",
+    cover: "",
+  },
+  {
+    id: 3,
+    title: "Third blog title 333",
+    category: "development",
+    description:
+      "<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>",
+    authorName: "Yunosuke Sato",
+    authorAvatar: "",
+    createdAt: "June 11, 2023",
+    cover: "",
+  },
+  {
+    id: 4,
+    title: "Fourth blog title 444",
+    category: "development",
+    description:
+      "<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>",
+    authorName: "Yunosuke Sato",
+    authorAvatar: "",
+    createdAt: "June 11, 2023",
+    cover: "",
+  },
+  {
+    id: 5,
+    title: "Fifth blog title 555",
+    category: "development",
+    description:
+      "<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>",
+    authorName: "Yunosuke Sato",
+    authorAvatar: "",
+    createdAt: "June 11, 2023",
+    cover: "",
+  },
+];
