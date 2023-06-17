8c5e59da90cfbf1b2bf874138960ba575509e187
Yunosuke Sato <yunosukesato04@gmail.com>
2023-06-17 14:29:36 -0700

    added routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+mongoose.connect(process.env.DATABASE_URI).then(() => {
+  console.log("Database Connected");
 });
 
-const port = 8000;
+app.use(express.json());
+app.use(cookieParser())
+app.use("/auth", authRoutes);
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
