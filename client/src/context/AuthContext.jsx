154d22666539bc7427bbbe0647de85b26b6ecfb1
Yunosuke Sato <yunosukesato04@gmail.com>
2023-06-17 18:14:56 -0700

    added AuthContext.js

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import { createContext, useContext } from "react";
+import useAuth from "../hooks/useAuth";
+
+const AuthContext = createContext();
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const { user, login, signup } = useAuth();
+
+  const value = {
+    user,
+    login,
+    signup,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
