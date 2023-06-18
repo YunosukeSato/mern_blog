623c30f2b6009ba94033b86aad078af808a6c5d7
Yunosuke Sato <yunosukesato04@gmail.com>
2023-06-17 18:19:14 -0700

    add login and signup button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Header from "../components/Home/Header";
 import SearchBar from "../components/Home/SearchBar";
+import axios from "../utils/axios";
 
 import { blogList } from "../testData/data";
 import BlogList from "../components/Home/BlogList/BlogList";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [blogs, setBlogs] = useState(blogList);
   const [searchKey, setSearchKey] = useState("All");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const checkStatus = async () => {
+      try {
+        await axios.get("/auth/user");
+
+        return setIsLoggedIn(true);
+      } catch (error) {
+        return setIsLoggedIn(false);
+      }
+    };
+
+    checkStatus();
+  });
+
+  const handleSignOut = async () => {
+    try {
+      await axios.delete("/auth/revoke_token");
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   // Search submit
   const handleSearchBar = (e) => {
@@ -27,7 +54,7 @@ function Home() {
 
   return (
     <>
-      <Header />
+      <Header isLoggedIn={isLoggedIn} handleSignOut={handleSignOut} />
       <SearchBar
         value={searchKey}
         formSubmit={handleSearchBar}
