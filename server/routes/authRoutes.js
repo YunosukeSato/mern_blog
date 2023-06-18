2842e5395f784d5fb75093248fe5a0452ddf83f0
Yunosuke Sato <yunosukesato04@gmail.com>
2023-06-17 18:16:57 -0700

    added revokeToken function

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup, login, user } = require("../controllers/authController");
-const verifyToken = require("../middleware/verifyToken")
+const {
+  signup,
+  login,
+  user,
+  revokeToken,
+} = require("../controllers/authController");
+const verifyToken = require("../middleware/verifyToken");
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/user",verifyToken, user)
+router.get("/user", verifyToken, user);
+router.delete("/revoke_token", revokeToken);
 
 module.exports = router;
