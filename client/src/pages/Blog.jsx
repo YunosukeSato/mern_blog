910223ad0703c48a64b3b8185486f801616b16e8
Yunosuke Sato <yunosukesato04@gmail.com>
2023-06-19 00:06:26 -0700

    Article page

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,21 +1,33 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { blogList } from "../testData/data";
 import Category from "../components/common/Category";
 import EmptyList from "../components/common/EmptyList";
 import { Link } from "react-router-dom";
+import DOMPurify from "dompurify";
 
+import axios from "../utils/axios";
 import defaultCover from "../assets/ER_bg_1.jpg";
+
+function sanitizeHTML(html) {
+  const sanitizedHTML = DOMPurify.sanitize(html);
+  return { __html: sanitizedHTML };
+}
 
 const Blog = () => {
   const { id } = useParams();
   const [blog, setBlog] = useState(null);
 
   useEffect(() => {
-    let blog = blogList.find((blog) => blog.id === parseInt(id));
-    if (blog) {
-      setBlog(blog);
-    }
+    const getArticle = async () => {
+      const response = await axios.get("/article/get/id", { params: { id } });
+
+      if (response.data.article == []) {
+        return console.log("Article not found");
+      }
+
+      return setBlog(response.data.article[0]);
+    };
+    getArticle();
   }, [id]);
 
   return (
@@ -30,7 +42,7 @@ const Blog = () => {
         <div className=" max-w-2xl my-0 mx-auto">
           <header className=" text-center">
             <p className=" text-sm text-[#a9a9a9] font-medium">
-              Published {blog.createdAt}
+              Published {blog.created_at}
             </p>
             <h1>{blog.title}</h1>
             <div className="flex justify-center">
@@ -39,12 +51,14 @@ const Blog = () => {
               </div>
             </div>
           </header>
-          <img
+          <img className="w-full rounded-lg" src={defaultCover} alt="cover" />
+          {/* <img
             className="w-full rounded-lg"
             src={blog.cover == "" ? defaultCover : blog.cover}
             alt="cover"
-          />
-          <p className="p-4 mt-6">{blog.description}</p>
+          /> */}
+          {/* <p className="p-4 mt-6">{blog.body}</p> */}
+          <div dangerouslySetInnerHTML={sanitizeHTML(blog.body)} />
         </div>
       ) : (
         <EmptyList />
