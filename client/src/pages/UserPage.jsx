cf29011c505d61c3d774c20ddd8420f18f2e0c48
Yunosuke Sato <yunosukesato04@gmail.com>
2023-06-19 00:06:50 -0700

    User page

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,93 +1,48 @@
 import { Link } from "react-router-dom";
-// import { useState } from "react";
-// import axios from "axios";
-// import { useSelector } from "react-redux";
-// import { RootState } from "../redux/rootReducer";
-// import { key } from "../key";
-import { blogList } from "../testData/data";
+import axios from "../utils/axios";
+import { useState, useEffect } from "react";
 
 import BlogList from "../components/Home/BlogList/BlogList";
-
-// const data = {
-//   username: "",
-//   jwt: "",
-//   status: false,
-// };
-
+import { useAuthContext } from "../context/AuthContext";
 
 function UserPage() {
-  // const userData = useSelector((state) => state.dataSlice);
+  const [articles, setArticles] = useState([]);
+  const user = useAuthContext();
 
-  // const [newData, setNewData] = useState(data);
-  // const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    const userid = user.user.userid;
+    const getArticle = async () => {
+      const response = await axios.get("/article/get/author", {
+        params: { userid },
+      });
 
-  // const getCookie = (param) => {
-  //   const cookies = document.cookie.split("; ");
-  //   const cookieString = cookies.find((cookie) =>
-  //     cookie.startsWith(`${param}=`)
-  //   );
-  //   return cookieString?.replace(`${param}=`, "");
-  // };
+      if (response.data.articles == []) {
+        return console.log("Article not found");
+      }
 
-  // useEffect(() => {
-  //   const checkStatus = async () => {
-  //     try {
-  //       // Getting username and token from cookie
-  //       const username = getCookie("username");
-  //       const token = getCookie("token");
+      return setArticles(response.data.articles);
+    };
+    getArticle();
+  }, [user.user.userid]);
 
-  //       // console.log(username, token);
-
-  //       const response = await axios.post(
-  //         "http://localhost:8000/user/check-status",
-  //         { username, token }
-  //       );
-
-  //       if (response.status === 200) {
-  //         return setNewData({
-  //           username: response.data.username,
-  //           jwt: response.data.token,
-  //           status: true,
-  //         });
-  //       }
-
-  //       setNewData(data);
-  //     } catch (err) {
-  //       setNewData(data);
-  //     }
-  //   };
-
-  //   checkStatus();
-  // }, [userData]);
-
-  // useEffect(() => {
-  //   const getArticles = async () => {
-  //     try {
-  //       const response = await axios.get(
-  //         "https://node-blog-api.herokuapp.com/articles/get/all",
-  //         {
-  //           headers: {
-  //             "Content-Type": "application/json",
-  //             Authorization: `Bearer ${key}`,
-  //           }
-  //         }
-  //       );
-  //       // console.log(response.data.articles[0]);
-  //       setArticles(response.data.articles[0]);
-  //       // console.log(articles);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-  //   getArticles();
-  // }, []);
   return (
     <>
       <div className="card mx-10 my-10 flex flex-row justify-between w-auto bg-[#D2E9E9] text-gray-900 shadow-xl">
-        <h1 className="m-10 text-3xl">Yunosuke Sato</h1>
-        <Link to="/" className="m-10 text-xl text-teal-500 hover:text-emerald-600">Home</Link>
+        <h1 className="m-10 text-3xl">{user.user.username}</h1>
+        <Link
+          to="/"
+          className="m-10 text-xl text-[#0080ff] hover:text-[#0f52ba]"
+        >
+          Home
+        </Link>
       </div>
-      {!blogList.length ? <h1>Empty</h1> : <BlogList blogs={blogList} />}
+      {!articles ? (
+        <div className="flex items-center justify-center">
+          Articles not found
+        </div>
+      ) : (
+        <BlogList blogs={articles} />
+      )}
     </>
   );
 }
